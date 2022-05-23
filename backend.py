200f07411865423d046c8450a871322999d71790
local_Fett00 <musaew2014@gmail.com>
2022-05-24 01:53:31 +0300

    convert string data from api to json structure

--- backend.py
+++ backend.py
@@ -1,24 +1,45 @@
-#import requests
+import requests
+import json
 
 #my code part
 
 class AppBackend:   
 
     def __init__(self):
-        pass
+        
+        str_json_data = NetworkWorker.request_json_data("https://www.themealdb.com/api/json/v1/1/categories.php")
+
+        json_data = json.loads(str_json_data)
+        print(json_data)
+
 
 
 
 class NetworkWorker:
 
-    import requests
+    @staticmethod
+    def request_json_data(from_url):
 
-    def request_data(from_url):
         response = requests.get(url=from_url)
 
-class JSONWorker:
+        if response.status_code == 200:
 
-    pass
+            try:
+
+                return response.text
+
+            except requests.exceptions.JSONDecodeError:
+
+                print("Failed to parse JSON")
+        else:
+
+            print("Error, requests status is: " + response.status_code)
+            return ""
+
 
 class TSBDWorker:
+    
     pass
+
+if __name__ == "__main__":
+    AppBackend()

68afa8fd5213dc904295c7174ca3bc084fcf25aa
local_Fett00 <musaew2014@gmail.com>
2022-05-29 16:31:08 +0300

    add auto sender

--- backend.py
+++ backend.py
@@ -44,7 +44,7 @@ class TSBDWorker:
 
     #url  "http://192.168.1.46:8086"
 
-    def sendDataToTSDB(self, data:dict, in_measure: str = "default"):
+    def sendDataToTSDB(self, json_data:dict, in_measure: str = "default"):
 
         import time
 
@@ -52,14 +52,13 @@ class TSBDWorker:
             
             with client.write_api(write_options= SYNCHRONOUS) as write_client:
 
-                for i in data:                                                  
+                for i in json_data:                                                  
                         
                     field = i
-                    value = data[i]
+                    value = json_data[i]
                     #print(value)
                     data = Point("json").measurement(in_measure).field(field, value)
                     write_client.write(self.__bucket, self.__org, data)
-                    time.sleep(1)
 
 
 class AppBackend:
@@ -73,11 +72,12 @@ class AppBackend:
     tsdb_sender = TSBDWorker()#TSBDWorker(ip_address = "")   
 
 
-    def get_data_and_convert_to_json(self):
+    def get_data_and_convert_to_json(self, ip_address:str):
 
-        str_json_data = NetworkWorker.request_json_data("http://192.168.1.46:8001")
+        str_json_data = NetworkWorker.request_json_data(ip_address)
         json_data = json.loads(str_json_data)
         out = self.flatten_data(json_data)
+        print("Successful get data and convert it to json")
         return out
         #print(json_data)
 
@@ -102,8 +102,21 @@ class AppBackend:
     def send_data_to_tsdb(self, data:dict, in_measure: str = "default"):
         
         self.tsdb_sender.sendDataToTSDB(data, in_measure= in_measure)
+        print("Successful send data to tsdb")
     
-    #def auto_sender(self, timeout=1):
+    def auto_sender(self, timeout=5):
+        
+        import time
+
+        if self.__json_ip == "" or self.__tsdb_ip == "":
+            return False
+
+        while True:
+
+            self.send_data_to_tsdb(self.get_data_and_convert_to_json(self.__json_ip))
+            time.sleep(timeout)
+
+
 
     def enter_tsdb_ip(self, ip_address):
         self.__tsdb_ip = ip_address
@@ -111,8 +124,14 @@ class AppBackend:
     def enter_json_ip(self, ip_address):
         self.__json_ip = ip_address
 
+    def get_flat_json_to_table(self):
+        pass
+
 if __name__ == "__main__":
     a = AppBackend()
-    json_data = a.get_data_and_convert_to_json()
-    a.send_data_to_tsdb(json_data)
+    a.enter_json_ip(input("EnterJsonIp: "))
+    a.enter_tsdb_ip(input("EnterTsdbIp: "))
+    a.auto_sender()
+    #json_data = a.get_data_and_convert_to_json()
+    #a.send_data_to_tsdb(json_data)
     

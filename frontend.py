eea87c97a9bbe96e9fddb23142e48e893243c88b
sketch666 <11@gmail.com>
2022-05-30 15:28:50 +0300

    1234

--- frontend.py
+++ frontend.py
@@ -3,36 +3,7 @@ import json
 import backend
 
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
-
-#from main import ViewTree
-
-class ViewTree(QTreeWidget):
-
-
-    def __init__(self, value = "{\"123\" = \"13\"}"):
-
-        super().__init__()
-        def fill_item(item, value):
-            def new_item(parent, text, val=None):
-                child = QTreeWidgetItem([text])
-                child.setFlags(child.flags() | Qt.ItemIsEditable)
-                fill_item(child, val)
-                parent.addChild(child)
-                child.setExpanded(True)
-            if value is None: return
-            elif isinstance(value, dict):
-                for key, val in sorted(value.items()):
-                    new_item(item, str(key), val)
-            elif isinstance(value, (list, tuple)):
-                for val in value:
-                    text = (str(val) if not isinstance(val, (dict, list, tuple))
-                            else '[%s]' % type(val).__name__)
-                    new_item(item, text, val)
-            else:
-                new_item(item, str(value))
-
-        fill_item(self.invisibleRootItem(), value)
+from PyQt5.QtCore import Qt, pyqtSlot
 
 
 class Window(QWidget):
@@ -44,14 +15,14 @@ class Window(QWidget):
 
         super().__init__()
         self.setWindowTitle("Kursach")
-        self.resize(339, 306)
+        self.resize(370, 350)
         # Create a top-level layout
         layout = QVBoxLayout()
         self.setLayout(layout)
         # Create the tab widget with two tabs
         tabs = QTabWidget()
         tabs.addTab(self.generalTabUI(), "General")
-    ##    tabs.addTab(self.networkTabUI(), "Network")
+
         layout.addWidget(tabs)
 
     def generalTabUI(self):
@@ -65,29 +36,52 @@ class Window(QWidget):
 
         # box1
         horizlayout1.addWidget(QLineEdit())
-        horizlayout1.addWidget(QPushButton("Get"))
+        buttonOne = QPushButton("Get")
+        horizlayout1.addWidget(buttonOne)
 
-        # TreeWidjet
-
-        #json_file = open("example.json", "r")
+        # action btn1
+        buttonOne.clicked.connect(self.on_click)  # соединение сигнала и слота (сигнал clicked и слот on_click)
+        # json_file = open("example.json", "r")
         ##TEMP##
-        #file = self.app_backend.get_data_and_convert_to_json()
-        formLayout1.addWidget(ViewTree())
+        # file = self.app_backend.get_data_and_convert_to_json()
+
+        # TableWidjet
+
+        tableWidjet = QTableWidget()
+        tableWidjet.setColumnCount(3)
+        tableWidjet.setRowCount(2)
+
+        tableWidjet.setHorizontalHeaderLabels(['CheckBox', 'Text', 'Text'])
+        tableWidjet.setItem(0, 1, QTableWidgetItem("Text1"))
+        tableWidjet.setItem(0, 2, QTableWidgetItem("Text3"))
+        tableWidjet.setItem(1, 2, QTableWidgetItem("Text4"))
+        tableWidjet.setItem(1, 1, QTableWidgetItem("Text2"))
+
+        tableWidjet.setCellWidget(0, 0, QCheckBox("Check1"))
+        tableWidjet.setCellWidget(1, 0, QCheckBox("Check2"))
+
+        formLayout1.addWidget(tableWidjet)
 
         # box3
         horizlayout2.addWidget(QLineEdit())
-        horizlayout2.addWidget(QPushButton("Get"))
+        button2 = QPushButton("Get")
+        horizlayout2.addWidget(button2)
+
+        #button2 action
+        button2.clicked.connect(self.on_clicko)  # соединение сигнала и слота (сигнал clicked и слот on_click)
 
         # засовываем горизонтальный бокс 1,2  в вертикальный бокс
         verticlayout1.addLayout(horizlayout1)
         verticlayout1.addLayout(formLayout1)
         verticlayout1.addLayout(horizlayout2)
-        verticlayout1.addWidget(QPushButton("Start"))
+        button3 = QPushButton("Start")
 
+        # button3 action
+        button3.clicked.connect(self.on_clickoff)  # соединение сигнала и слота (сигнал clicked и слот on_click)
 
+        verticlayout1.addWidget(button3)
 
         generalTab.setLayout(layout)
-
 
         # Set the layout on the dialog
         layout.addLayout(verticlayout1)
@@ -96,24 +90,37 @@ class Window(QWidget):
 
         return generalTab
 
+    def on_click(self):
+            print('Clicked!')
+
+    def on_clicko(self):
+            print('Wake up')
+
+    def on_clickoff(self):
+            print('NYAAAA')
+
+
+
 
 """
 1) ПОменять виджет дерева на виджет таблицы с тремястолбцами: столбец с чекбоксом, и два текстовых
 2) Добавить функию для реакции на нажатия на кнопки
 ЗАПУШИТЬ!!!!!!!!НА ГИТХАБ!!!!!
+
 """
 
    # def networkTabUI(self):    вторая страничка
-        """Create the Network page UI."""
+        # Create the Network page UI."""
     #    layout = QVBoxLayout()
         #    layout.addWidget(QCheckBox("Network Option 1"))
         #   layout.addWidget(QCheckBox("Network Option 2"))
         #   networkTab.setLayout(layout)
     #   return networkTab
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
     window.show()
     sys.exit(app.exec_())
-    print(1231)
+

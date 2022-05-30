07d36f81d5a2f4b6f63929b9739a752da63be1b0
local_Fett00 <musaew2014@gmail.com>
2022-05-30 15:01:53 +0300

    add independent work for backend

--- backend.py
+++ backend.py
@@ -44,11 +44,11 @@ class TSBDWorker:
 
     #url  "http://192.168.1.46:8086"
 
-    def sendDataToTSDB(self, json_data:dict, in_measure: str = "default"):
+    def sendDataToTSDB(self, json_data:dict, url:str, in_measure: str = "default"):
 
         import time
 
-        with InfluxDBClient(url= self.__url, token= self.__token, org= self.__org) as client:
+        with InfluxDBClient(url= url, token= self.__token, org= self.__org) as client:
             
             with client.write_api(write_options= SYNCHRONOUS) as write_client:
 
@@ -56,15 +56,20 @@ class TSBDWorker:
                         
                     field = i
                     value = json_data[i]
+                    print(field)
                     #print(value)
                     data = Point("json").measurement(in_measure).field(field, value)
-                    write_client.write(self.__bucket, self.__org, data)
+                    try:
+                        write_client.write(self.__bucket, self.__org, data)
+                    except:
+                        print("Write failed cancel and try again")
+                        break
 
 
 class AppBackend:
 
-    __json_ip = ""
-    __tsdb_ip = ""
+    json_ip = ""
+    tsdb_ip = ""
 
     def __init__(self):
         pass
@@ -99,39 +104,103 @@ class AppBackend:
         flatten(y)
         return out
     
-    def send_data_to_tsdb(self, data:dict, in_measure: str = "default"):
+    def send_data_to_tsdb(self, data:dict, url:str, in_measure: str = "default"):
         
-        self.tsdb_sender.sendDataToTSDB(data, in_measure= in_measure)
-        print("Successful send data to tsdb")
+        self.tsdb_sender.sendDataToTSDB(data, url=url, in_measure= in_measure)
     
-    def auto_sender(self, timeout=5):
+    def auto_sender(self, data_key_list:list, timeout=5):
         
         import time
 
-        if self.__json_ip == "" or self.__tsdb_ip == "":
+        if self.json_ip == "" or self.tsdb_ip == "":
             return False
 
         while True:
 
-            self.send_data_to_tsdb(self.get_data_and_convert_to_json(self.__json_ip))
-            time.sleep(timeout)
+            data = self.get_data_and_convert_to_json(self.json_ip)
+
+            if data != None or data != {}:
+                
+                corrected_data = {}
+
+                for i in data_key_list:
+                    corrected_data[i] = data[i]
+
+                self.send_data_to_tsdb(corrected_data, url=self.tsdb_ip)
+                time.sleep(timeout)
+            else:
+                print("Error: no data to send")
 
 
 
     def enter_tsdb_ip(self, ip_address):
-        self.__tsdb_ip = ip_address
+        self.tsdb_ip = str(ip_address)
     
     def enter_json_ip(self, ip_address):
-        self.__json_ip = ip_address
+        self.json_ip = str(ip_address)
 
     def get_flat_json_to_table(self):
         pass
 
 if __name__ == "__main__":
+
     a = AppBackend()
-    a.enter_json_ip(input("EnterJsonIp: "))
-    a.enter_tsdb_ip(input("EnterTsdbIp: "))
+
+    while True:
+        try:
+            json_ip = input("Enter Json Ip: ")
+            tsdb_ip = input("Enter Tsdb Ip: ")
+            requests.get(json_ip)
+            a.enter_json_ip(json_ip)
+            a.enter_tsdb_ip(tsdb_ip)
+            break
+        except:
+            print("Try again!")
+
+    data = a.get_data_and_convert_to_json(a.json_ip)
+
+    if data != None or data != {}:
+        print("Получен json файл. Кол-во полей в котором равно: ", len(data))
+        print("Выбирете способ выбора полей для отправки в БД:\n1) Отправить все\n2) Указать диапазон\n3) Указать номер конкретного поля")
+        choosen_number = int(input())
+        
+        while True:
+            if choosen_number == 1:
+                a.auto_sender(list(data))
+                break
+            elif choosen_number == 2:
+
+                n = None
+                m = None
+                
+                while True:
+                    print("Введите значения n и m где m и n целые числа диапазона и n<m и n >= 1")
+                    try:
+                        n = int(input("n: ")) - 1
+                        m = int(input("m: ")) - 1
+
+                        if (n < m) and (n in range(0, len(data))) and (m in range(0, len(data))):
+                            break
+                        else:
+                            raise ValueError("Error")
+                    except:
+                        print("Неправильные данные попробуйте еще раз")
+                a.auto_sender(list(data)[n:m])
+                break
+            elif choosen_number == 3:
+
+                n = None
+
+                while True:
+                    print("Введите значения n где n целое число и n >= 1")
+                    n = int(input("n: ")) - 1
+                    if (n in range(0, len(data))):
+                        break
+                    else:
+                        print("Неправильные данные попробуйте еще раз")
+                a.auto_sender([(list(data)[n])])
+                break
+            else:
+                print("Пожалуйста введите корректный номер: ")
     a.auto_sender()
-    #json_data = a.get_data_and_convert_to_json()
-    #a.send_data_to_tsdb(json_data)
     
